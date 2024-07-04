5e9f61c803cd1cf4391c31e37ffd1face5beb24b
I_don't_tell_u <133332005+MaiBok123@users.noreply.github.com>
2024-07-04 17:35:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,7 @@
-var unlockedWeeks = [];
+document.addEventListener('DOMContentLoaded', function() {
+    const soundEffect = document.getElementById('soundEffect');
 
-function toggleContent(id, week) {
-    if (!unlockedWeeks.includes(week)) {
-        alert('คุณยังไม่สามารถเปิดดูเนื้อหาของสัปดาห์นี้ได้');
-        return;
-    }
-    
-    var content = document.getElementById(id);
-    if (content.style.display === "none") {
-        content.style.display = "block";
-    } else {
-        content.style.display = "none";
-    }
-}
-
-unlockWeek(1);
-unlockWeek(2);
-unlockWeek(3);
-lockWeek(4);
-
-function unlockWeek(week) {
-    if (!unlockedWeeks.includes(week)) {
-        unlockedWeeks.push(week);     
-    }
-}
-
-function lockWeek(week) {
-    var index = unlockedWeeks.indexOf(week);
-    if (index !== -1) {
-        unlockedWeeks.splice(index, 1);
-    }
-}
+    document.querySelector('header').addEventListener('mouseover', function() {
+        soundEffect.play();
+    });
+});
