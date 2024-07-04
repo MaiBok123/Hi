89db606078c3c14fdf4eff1c714a1499e6f89c57
I_don't_tell_u <133332005+MaiBok123@users.noreply.github.com>
2024-07-04 17:48:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const soundEffect = document.getElementById('soundEffect');
+    const textElement = document.getElementById('shuffled-text');
+    const finalText = textElement.innerText;
+    let interval;
 
+    function getRandomChar() {
+        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        return chars[Math.floor(Math.random() * chars.length)];
+    }
+
+    function shuffleText(element, text) {
+        let iteration = 0;
+        interval = setInterval(() => {
+            element.innerText = text
+                .split('')
+                .map((char, index) => {
+                    if (index < iteration) {
+                        return text[index];
+                    }
+                    return getRandomChar();
+                })
+                .join('');
+
+            if (iteration >= text.length) {
+                clearInterval(interval);
+            }
+
+            iteration += 1 / 3;
+        }, 30);
+    }
+
+    shuffleText(textElement, finalText);
+
+    const soundEffect = document.getElementById('soundEffect');
     document.querySelector('header').addEventListener('mouseover', function() {
         soundEffect.play();
     });
